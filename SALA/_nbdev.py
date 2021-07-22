e481fd04f817e186c2c22c6008e92274bfc72d2b
Tyler Tran <tktran11@yahoo.com>
2021-07-22 13:14:01 -0700

    added unfinished clock plots, needs html fixing

--- SALA/_nbdev.py
+++ SALA/_nbdev.py
@@ -5,9 +5,14 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"get_raw_data": "00_analyze_by_person.ipynb",
          "process_timing_data": "00_analyze_by_person.ipynb",
          "set_sun_timings": "00_analyze_by_person.ipynb",
-         "process_sleep_data": "00_analyze_by_person.ipynb"}
+         "process_sleep_data": "00_analyze_by_person.ipynb",
+         "export_timing_data": "00_analyze_by_person.ipynb",
+         "map_mins_to_rads": "clock_plots.ipynb",
+         "time_print": "clock_plots.ipynb",
+         "make_clock_plots": "clock_plots.ipynb"}
 
-modules = ["analyze_by_person.py"]
+modules = ["analyze_by_person.py",
+           "clock_plots.py"]
 
 doc_url = "https://tktran11.github.io/SALA/"
 
